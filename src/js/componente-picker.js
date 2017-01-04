afe4ba0bc6b00f1ab39000b2b2df3a5df1315207
susan-TG <susana.tralma.g@gmail.com>
2017-01-04 00:04:37 -0300

    archivos

---
+++ src/js/componente-picker.js
@@ -0,0 +1,17 @@
+$('.datepicker').pickadate({
+		    selectMonths: true, // Creates a dropdown to control month
+		    selectYears: 15 // Creates a dropdown of 15 years to control year
+		  });
+
+		$(document).ready(function() {
+    		$('select').material_select();
+
+    		$('input.autocomplete').autocomplete({
+			    data: {
+			      "Arica": null,
+			      "Iquique": null,
+			      "Calama": null
+			    }
+	  		});
+
+  		});
